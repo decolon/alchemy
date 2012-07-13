5e19998837d81c772e563de92244cc1487bdf5de
Daniel Colon <decolon13@gmail.com>
2012-07-12 18:13:00 -0700

    first alchemy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alchemy::Application.config.secret_token = '219d120266b2d9092b2d49d7f282aa8abd121d41978d4107d2baf69f0c556f8c117c602bb888849a75563e9c16b6dfd95c2cc49805664fbc773320147a8ac062'
